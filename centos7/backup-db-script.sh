7da9b392d05054bc7ed37ac585a82871de1a0719
Hy Nguyen <hynguyen2610@gmail.com>
2024-06-18 11:19:59 +0700

    Parameterize s3 URL

--- centos7/backup-db-script.sh
+++ centos7/backup-db-script.sh
@@ -9,21 +9,22 @@ DB_PASSWORD=$2
 DB_NAME=$3
 AWS_ACCESS_KEY_ID=$4
 AWS_SECRET_ACCESS_KEY=$5
+BUCKET_NAME=$6
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <DB_HOST> <DB_PASSWORD> <DB_NAME> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <DB_HOST> <DB_PASSWORD> <DB_NAME> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <BUCKET_NAME>"
     exit 1
 fi
 
-BACKUP_FILE_NAME=/tmp/db_backup_$DATE.sql
+BACKUP_FILE_NAME=db_$DATE.sql
 
 # Perform the database backup
-mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE_NAME
+mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP_FILE_NAME
 
 # Check if the mysqldump command was successful
 if [ $? -eq 0 ]; then
-    echo "Database backup executed successfully: /tmp/db_backup_$DATE.sql"
+    echo "Database backup executed successfully: /tmp/db_$DATE.sql"
 else
     echo "Database backup failed"
     exit 1
@@ -35,7 +36,8 @@ export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 # Copy the back up file to S3
-aws s3 cp $BACKUP_FILE_NAME s3://jenkins-mysql-backup-ndh/$BACKUP_FILE_NAME
+S3_BACKUP_PATH=backup
+aws s3 cp /tmp/$BACKUP_FILE_NAME $BUCKET_NAME/$S3_BACKUP_PATH/$BACKUP_FILE_NAME
 
 if [ $? -eq 0 ]; then
     echo "Backup file uploaded successfully to S3"
