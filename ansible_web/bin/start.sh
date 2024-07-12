5958bc2b3f2f1a509618934f27ed0ae6e019fa62
Hy Nguyen <hynguyen2610@gmail.com>
2024-07-12 17:16:57 +0700

    php and nginx works in web container startup

--- ansible_web/bin/start.sh
+++ ansible_web/bin/start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Start ssh daemon
+echo "Starting sshd..."
+
+# Start PHP-FPM in the foreground
+echo "Starting php-fpm..."
+php-fpm -F -R -c /etc/php/fpm &
+
+# Start Nginx in the foreground
+echo "Starting nginx..."
+nginx -g "daemon off;"
+
+# Start SSH daemon if needed (commented out by default)
 /usr/sbin/sshd -D
-
-# Start the php process in background
-php-fpm -c /etc/php/fpm
-
-# Start nginx foregr
-nginx
