77b59d4781b3e25395082f7ae75e03ef4d9ed044
Hy Nguyen <hynguyen2610@gmail.com>
2024-07-15 12:14:17 +0700

    Correct order of commands in start.sh

--- ansible_web/bin/start.sh
+++ ansible_web/bin/start.sh
@@ -8,7 +8,9 @@ php-fpm -F -R -c /etc/php/fpm &
 
 # Start Nginx in the foreground
 echo "Starting nginx..."
-nginx -g "daemon off;"
 
 # Start SSH daemon if needed (commented out by default)
-/usr/sbin/sshd -D
+/usr/sbin/sshd
+
+nginx -g "daemon off;"
+
